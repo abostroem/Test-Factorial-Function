1bbfc2686fac83dfc67e460b9678b8d5fe7a13ad
Greg Wimsatt <gwwimsatt@ucdavis.edu>
2016-02-10 16:46:09 -0800

    So many problems to fix...

--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -1,2 +1,2 @@
 def test_factorial_function():
-  print "How's you day going?  You said fine?  Okay.  Everything is fine!"
+  print("How's you day going?  You said fine?  Okay.  Everything is fine!")

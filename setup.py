5d004494fc734731689e03d70964c9315032dee0
Greg Wimsatt <gwwimsatt@ucdavis.edu>
2016-02-17 16:39:14 -0800

    Added some setup files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='FactorialPackage',
+    version='0.1dev',
+    packages=['factorial',],
+    license='GPL3',
+    long_description=open('README.md').read(),
+)
+

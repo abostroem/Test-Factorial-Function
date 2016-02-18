43e1593dde9d5bdc8573a1344386f2dba4d12cb0
Greg Wimsatt <gwwimsatt@ucdavis.edu>
2016-02-17 16:56:31 -0800

    added stuff to tests

--- factorial.py
+++ factorial.py
@@ -1,1 +1,30 @@
-def factorial(n): return 1 if n==0 else n*factorial(n-1)
+def factorial(n):
+    """
+    Function that takes a nonzero integer and returns its factorial.  Its the
+    main function in my repo used for our good coding and project practice
+    meetings.
+    """
+
+    # First make sure the input is a number
+    try:
+        n+1
+    except:
+        raise AssertionError("n is not a number")
+
+    # Then make sure it is a nonnegative integer
+    assert n%1 == 0, "not an integer"
+    assert n>=0, 'int must be non-negative'
+
+    # Now execute the algorithm for finding the factorial
+    # Return 1 for the case that the input is 0
+    if n == 0:
+        return 1
+
+    # For positive input, loop over all integers up to the input, multiplying
+    # the integers by a stored value, starting at 1.
+    answer = 1
+    for i in range(1,n+1):
+        answer = i*answer
+
+    # Return the answer for positive integer input
+    return answer
--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -1,4 +1,23 @@
+# import the function to test and some nosetools tools
 from factorial import factorial
+from nose.tools import assert_equal, assert_raises
 
-def test_factorial_function():
-  assert factorial(1) == 1
+# Here we define several good pairs of inputs and outputs to test
+good_input_output_pairs = {
+  0: 1,
+  1: 1,
+  10: 3628800,
+}
+
+# Now we run the tests to make sure the good inputs are followed by good outputs
+def test_factorial_good_values():
+    for arg, val in good_inputs.items():
+        yield assert_equal, factorial(arg), val
+
+# Define some inputs that should be caught as bad by the function
+bad_inputs = [1.5, -10, -1.5, None, test_factorial_function, 'pants', '3']
+
+# Now run the tests to make sure the bad inputs are caught
+def test_factorial_for_bad_input():
+    for bad in bad_inputs:
+        yield assert_raises, AssertionError, factorial, bad
